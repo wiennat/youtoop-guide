40083987cd6568169c240431a9edfd5f145aa87e
wiennat <wiennat@gmail.com>
2019-05-30 01:11:49 +0900

    add db to docker-compose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,30 @@
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-import '@babel/polyfill';
-import express from 'express';
-import chalk from 'chalk';
-import path from 'path';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
-import compression from 'compression';
-import logger from './logger';
-import knex from 'knex';
-import JsonDataSource from './lib/JsonDataSource';
-import JsonNormalize from './lib/JsonNormalizer';
-import MySqlDataSource from './lib/MySqlDataSource';
-import MySqlNormalizer from './lib/MySqlNormalizer';
+import "@babel/polyfill";
+import express from "express";
+import chalk from "chalk";
+import path from "path";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import compression from "compression";
+import logger from "./logger";
+import knex from "knex";
+import JsonDataSource from "./lib/JsonDataSource";
+import JsonNormalize from "./lib/JsonNormalizer";
+import MySqlDataSource from "./lib/MySqlDataSource";
+import MySqlNormalizer from "./lib/MySqlNormalizer";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const dataPath = process.env.DATA_PATH || path.join(__dirname, './data');
+const dataPath = process.env.DATA_PATH || path.join(__dirname, "./data");
+logger.debug("initializing db with following settings");
+logger.debug(`host: ${process.env.DB_HOST}`);
+logger.debug(`user: ${process.env.DB_USER}`);
+logger.debug(`password: ***`);
+logger.debug(`database: ${process.env.DB_NAME}`);
 const knexClient = knex({
-  client: 'mysql',
+  client: "mysql",
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -27,8 +32,24 @@ const knexClient = knex({
     database: process.env.DB_NAME,
     charset: "utf8mb4"
   },
+  pool: {
+    afterCreate: (conn, done) => {
+      // make a test query to ensure connection
+      logger.info("connection created");
+      done(null, conn);
+    }
+  },
   debug: process.env.DEBUG
 });
+
+knexClient
+  .raw("SELECT 1+1;")
+  .then(() => {
+    logger.info("db inited");
+  })
+  .catch(err => {
+    logger.error("cannot make connection", err);
+  });
 
 // const storyDatasource = new JsonDataSource(path.join(dataPath, 'data.json'));
 const storyDatasource = new MySqlDataSource({
@@ -42,85 +63,98 @@ const analytics = {
   enabled: process.env.ANALYTICS_ENABLED === "true"
 };
 
-app.set('views', path.join(__dirname, './pages'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "./pages"));
+app.set("view engine", "ejs");
 
-const getRemoteAddress = (req) => {
-  return req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-}
-morgan.token('remote-addr', getRemoteAddress);
+const getRemoteAddress = req => {
+  return (
+    req.headers["x-real-ip"] ||
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress
+  );
+};
+morgan.token("remote-addr", getRemoteAddress);
 
-app.use(morgan(':method :url HTTP/:http-version :status :res[content-length] :remote-addr - :remote-user', {
-  stream: logger.stream
-}));
+app.use(
+  morgan(
+    ":method :url HTTP/:http-version :status :res[content-length] :remote-addr - :remote-user",
+    {
+      stream: logger.stream
+    }
+  )
+);
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.get('/', (req, res) => {
-  return res.render('index', {
+app.get("/", (req, res) => {
+  return res.render("index", {
     rawKeywords: "",
     stories: [],
     analytics
   });
 });
 
-app.get('/search', (req, res) => {
-  res.redirect('/');
+app.get("/search", (req, res) => {
+  res.redirect("/");
 });
 
-app.get('/search/:keyword', (req, res) => {
-  const tokens = req.params.keyword.trim().split(' ');
-  const keyword = req.params.keyword;  
-  logger.info('search: %s - %s', keyword, getRemoteAddress(req));
-  filterDatasource.normalize(req.params.keyword)
-    .then((keywords) => {
-      storyDatasource.search(keywords)
-        .then((stories) => res.render('index', {
-          stories,
-          rawKeywords: req.params.keyword,
-          analytics
-        }))
-    });
+app.get("/search/:keyword", (req, res) => {
+  const tokens = req.params.keyword.trim().split(" ");
+  const keyword = req.params.keyword;
+  logger.info("search: %s - %s", keyword, getRemoteAddress(req));
+  filterDatasource.normalize(req.params.keyword).then(keywords => {
+    storyDatasource.search(keywords).then(stories =>
+      res.render("index", {
+        stories,
+        rawKeywords: req.params.keyword,
+        analytics
+      })
+    );
+  });
 });
 
-app.post('/api/search', (req, res) => {
+app.post("/api/search", (req, res) => {
   const keyword = req.body.keyword;
   if (keyword.trim().length === 0) {
     return res.send([]);
   }
 
-  logger.info('api search: %s - ', keyword, getRemoteAddress(req));
-  const f = filterDatasource.normalize(keyword)
-    .then((keywords) => {
-      storyDatasource.search(keywords)
-        .then((stories) => res.send(stories));
-    });
+  logger.info("api search: %s - ", keyword, getRemoteAddress(req));
+  const f = filterDatasource.normalize(keyword).then(keywords => {
+    logger.debug(
+      "normalized : %s > %s %s",
+      keyword,
+      keywords,
+      getRemoteAddress(req)
+    );
+    storyDatasource.search(keywords).then(stories => res.send(stories));
+  });
 });
 
-app.post('/api/open', (req, res) => {
-  const {
-    ep,
-    url,
-    keyword
-  } = req.body;
+app.post("/api/open", (req, res) => {
+  const { ep, url, keyword } = req.body;
   const ip = getRemoteAddress(req);
-  logger.info('open: (%s), (%s), (%s), (%s)', ep, keyword, ip, url);
+  logger.info("open: (%s), (%s), (%s), (%s)", ep, keyword, ip, url);
   return res.send("ok");
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // error handler
-app.use(function (err, req, res, next) {
-  logger.error(chalk.red(`${err.message}
-  ${err.stack}`));
+app.use(function(err, req, res, next) {
+  logger.error(
+    chalk.red(`${err.message}
+  ${err.stack}`)
+  );
   res.status(err.status || 500).send(err.message);
 });
 
 app.listen(PORT, () => {
-  logger.debug(chalk.green(`Server listening on http://localhost:${PORT}/ ..`))
+  logger.debug(chalk.green(`Server listening on http://localhost:${PORT}/ ..`));
 });
 
 export default app;
